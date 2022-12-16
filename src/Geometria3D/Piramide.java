e6fdd3ef4baf1a630dac1a901434279e738ab863
Nestor <nestor.chumania@epn.edu.ec>
2022-12-16 16:13:18 -0500

    Terminando metodos de la Piramide

--- src/Geometria3D/Piramide.java
+++ src/Geometria3D/Piramide.java
@@ -1,4 +1,33 @@
 package Geometria3D;
 
 public class Piramide {
+    private double ladoBase, altura;
+
+    public Piramide(double ladoBase, double altura) {
+        this.ladoBase = ladoBase;
+        this.altura = altura;
+    }
+
+    public double getLadoBase() {
+        return ladoBase;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    //Area de la Base
+    public double areaBase(Piramide piramide){
+        return Math.pow(piramide.getLadoBase() ,2);
+    }
+
+    //Perimetro de la Base
+    public double perimetroBase(Piramide piramide){
+        return (piramide.getLadoBase() * 4) / 2;
+    }
+
+    //Volumen de la Base
+    public double volumenPiramide(Piramide piramide){
+        return (Math.pow(piramide.getLadoBase(),2) * piramide.getAltura())/3;
+    }
 }
